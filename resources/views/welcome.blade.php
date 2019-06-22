0193d49de56d9026c49c84e3d5344c8b69bd3ce1
Frank Egan <trkfrank@gmail.com>
2019-06-22 16:55:42 -0400

    Modify splash page template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,17 +81,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Iversoft Test App
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="/users">Users</a>
+                    <a href="/user_roles">Roles</a>
+                    <a href="/user_addresses">Addresses</a>
                 </div>
             </div>
         </div>
