cb83513ddc811cf1bb09a55b3e2daf263ee62c19
Frank Egan <trkfrank@gmail.com>
2019-06-22 16:54:45 -0400

    Register routes for controllers

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resources([
+    'users' => 'UserController',
+    'user_roles' => 'UserRoleController',
+    'user_addresses' => 'UserAddressController'
+]);
